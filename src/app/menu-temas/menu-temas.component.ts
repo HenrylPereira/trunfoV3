d08d5a2e2c13575ecca445742562c366cb45a664
Henry <henry.pereira@univille.br>
2023-04-17 13:00:21 -0300

    ata

--- src/app/menu-temas/menu-temas.component.ts
+++ src/app/menu-temas/menu-temas.component.ts
@@ -9,19 +9,39 @@ import { Router } from '@angular/router';
 export class MenuTemasComponent implements OnInit {
 
   public backgroundImage: string = "url('https://imgur.com/55Wl0dX.png')";
+  public bckColor = "#f2f2f2";
 
   public cards = [
-    {cardImage: 'https://imgur.com/55Wl0dX.png', backgroundImage: 'https://w0.peakpx.com/wallpaper/916/847/HD-wallpaper-homem-aranha-filmes-herois-homem-aranha-marvel-spider-man.jpg', tema: 'heros', id: 1},
-    {cardImage: 'https://imgur.com/DCHUcKG.png', backgroundImage: 'https://i.pinimg.com/originals/c8/fc/0e/c8fc0e6a7e5745fab7531699e8a30fad.jpg', tema: 'cars', id: 2},
-    {cardImage: 'https://imgur.com/ohRLf7M.png', backgroundImage: 'https://imgur.com/ohRLf7M.png', tema: 'soccer', id: 3}
+    {
+      cardImage: 'https://imgur.com/55Wl0dX.png',
+      backgroundImage: 'https://w0.peakpx.com/wallpaper/916/847/HD-wallpaper-homem-aranha-filmes-herois-homem-aranha-marvel-spider-man.jpg',
+      tema: 'heros',
+      color:"#69007351",
+      id: 1
+    },
+    {
+      cardImage: 'https://imgur.com/DCHUcKG.png',
+      backgroundImage: 'https://i.pinimg.com/originals/c8/fc/0e/c8fc0e6a7e5745fab7531699e8a30fad.jpg',
+      tema: 'cars',
+      color:"#73001051",
+      id: 2
+    },
+    {
+      cardImage: 'https://imgur.com/ohRLf7M.png',
+      backgroundImage: 'https://pbs.twimg.com/media/EtdjOCrXAAgn9bd.jpg',
+      tema: 'soccer',
+      color:"#00733051",
+      id: 3
+    }
   ];
 
   constructor(private router: Router) {}
 
   ngOnInit(): void {}
 
-  changeTheme(src: string) {
-    this.backgroundImage = `url(${src})`;
+  changeTheme(card: any) {
+    this.backgroundImage = `url(${card.backgroundImage})`;
+    this.bckColor = card.color;
   }
 
   public selecionar(){
